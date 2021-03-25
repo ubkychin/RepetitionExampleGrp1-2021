27f2b3ea61ba133e7d8e9a7261247feffc469d27
Anh Nguyen <anguyen@swin.edu.au>
2021-03-25 16:17:42 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RepetitionExampleGrp1 {
+    class Program {
+        static void Main(string[] args) {
+            
+            
+            
+            System.Console.Write("Please enter number of times to repeat: ");
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+
+            int counter = 0;
+
+
+            while ( counter < number) {
+                System.Console.Write("Please enter a name: ");
+                string input = Console.ReadLine();
+            
+                System.Console.WriteLine(input);
+                
+                // counter = counter + 1
+                counter++;
+            }
+
+            System.Console.WriteLine("Goodbye");
+        }
+    }
+}
